21c659eb8f57427222762c9ff4fd15eb636c844c
unknown <mandms337>
2025-01-28 11:07:33 +0300

    tgbot

--- tgbot.py
+++ tgbot.py
@@ -1,12 +1,21 @@
+import logging
+
 import telebot
+from telebot.types import Message
+
 
 bot = telebot.TeleBot('7581915376:AAG8FWud6rpdfM7tRhVgaWmz19-AeX9Gogg')
 
 @bot.message_handler(commands=['start'])
-def main(message):
+def cmd_start(message: Message):
     bot.send_message(message.chat.id,'Привет , опять тупишь , чем помочь , гений?')
 
-bot.polling(none_stop=True)
+
+@bot.message_handler(content_types=['text'])
+def any_text(message: Message):
+    bot.send_message(message.chat.id, str(reversed(message.text)))
 
 
-
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    bot.polling(none_stop=True, logger_level=logging.INFO)
